aa8544724c62b2f28865ea6d8d983f146a9ab684
alex-avila <alex.avilx@icloud.com>
2018-06-25 20:51:09 -0600

    Makes project ready for deployment

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux'
 import store from './redux'
 
 // Router
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 
 // Components
 import App from './App';
