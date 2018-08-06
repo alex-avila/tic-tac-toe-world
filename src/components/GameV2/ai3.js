0c308259aeb965f60d31ba7431587731a4a41f35
alex-avila <alex.avilx@icloud.com>
2018-08-06 17:29:21 -0600

    Remove unnecessary and comments

--- src/components/GameV2/ai3.js
+++ src/components/GameV2/ai3.js
@@ -20,9 +20,6 @@ let ai = {
       ['topM', 'midM', 'botM'],
       ['topR', 'midR', 'botR']
     ]
-    // only checking if 'o' wins because
-    // it's impossible for 'o' to make a move
-    // and end up losing on this turn
     const boardIsFull = !this.findAvailableMoves(board).length
     if (boardIsFull) {
       for (let i = 0; i < winConditions.length; i++) {
@@ -30,7 +27,7 @@ let ai = {
         let won =
           board[a] === target && board[b] === target && board[c] === target
         if (won) {
-          return 10 // win
+          return 10
         }
       }
       return 5
@@ -40,7 +37,7 @@ let ai = {
         let won =
           board[a] === target && board[b] === target && board[c] === target
         if (won) {
-          return 10 // win
+          return 10
         }
       }
     }
@@ -50,32 +47,13 @@ let ai = {
       let won =
         board[a] === target && board[b] === target && board[c] === target
       if (won) {
-        return 10 // win
+        return 10
       }
     }
     return
   },
 
   minimax(board, moves, max) {
-    // STEPS //
-    // 1a start as max
-    // 2a loop through moves
-    // 3a send each board with the current move to getState
-    // 4a if getState tells us we win, add '1' to the array // important //
-    // 5a if getState tells us nothing, don't alter the array
-    // and run minimax again as the max = false, so it's min's turn
-
-    // 1b start as min
-    // 2b loop through new moves
-    // 3b send each newBard with the current move to getState
-    // 4b if getState tells us we win, add '-1' to the array // important //
-    // 5b if getSTate tells us nothing,
-
-    // 6 go back to step one ...and so on... and so on... and so on...
-
-    // getState should check if the board is full
-    // if it doesn't see a win, it should return a '0' // important //
-
     let target
     let results = []
     if (max) {
@@ -85,7 +63,7 @@ let ai = {
         const newBoard = { ...board, [move]: target }
         let result = this.getState(newBoard, target)
         if (result === 10) {
-          results.push(10) // win
+          results.push(10)
         } else if (result === 5) {
           results.push(5)
         } else {
@@ -123,3 +101,22 @@ let ai = {
 }
 
 export default ai
+
+// MINIMAX STEPS //
+// 1a start as max
+// 2a loop through moves
+// 3a send each board with the current move to getState
+// 4a if getState tells us we win, add '1' to the array // important //
+// 5a if getState tells us nothing, don't alter the array
+// and run minimax again as the max = false, so it's min's turn
+
+// 1b start as min
+// 2b loop through new moves
+// 3b send each newBard with the current move to getState
+// 4b if getState tells us we win, add '-1' to the array // important //
+// 5b if getSTate tells us nothing,
+
+// 6 go back to step one ...and so on... and so on... and so on...
+
+// getState should check if the board is full
+// if it doesn't see a win, it should return a '0' // important //
